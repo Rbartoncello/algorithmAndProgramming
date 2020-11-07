eff4c2c2b044a1c108613d9e50995d590934b90b
rbartoncello <rbartoncello@indec.gob.ar>
2020-11-07 20:12:23 -0300

    fifth commit

--- unit_2/exercise6.c
+++ unit_2/exercise6.c
@@ -1,42 +1,50 @@
 #include<stdio.h>
-#include <string.h>
-#include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
 
 #define MSG_USER "Ingrese un texto"
+#define MSG_USER_END "Lo ingresado fue"
+#define MAX_STR 100
+
+
 
 int main(void){
-    char date;
-    size_t len;
+    int num=0, althnum=0, no_althnu=0;
+    char c, sentence[MAX_STR];
+    size_t i = 0;
 
     printf("%s\n", MSG_USER);
-    scanf("%s", &date);
-
-    len=strlen(&date);
-    printf("La cantidad de caracteres totales es: %lu\n", len);
-
-    if(isalnum(date) == 0)
+    while ((c = getchar()) !='\n')
     {
-        printf("No es alfanumerico\n");
-        printf("La cantidad de caracteres no alfanumeticos es: %lu\n", len);
-
-    } else
-    {
-        printf("Hay alguna letra o numero\n");
-        printf("La cantidad de caracteres alfanumeticos es: %lu\n", len);
+        if(isalnum(c))
+            {
+                ++althnum;
+                
+            } else
+            {
+                ++no_althnu;
+            }
+            
+        if(isdigit(c))
+            {
+                ++num;        
+            }
+        sentence[i++] = c;
     }
-    if(isdigit(date) == 0)
-    {
-        printf("Hay numeros y letra o solo letras\n");
-    } else
-    {
-        printf("Solamente hay numeros\n");
-        printf("La cantidad de caracteres numericos es: %lu\n", len);
-    }
+    printf("La cantidad de caracteres alfanumericos son: %i\n", althnum);
+    printf("La cantidad de caracteres numericos son: %i\n", num);
+    printf("La cantidad de caracteres no alfanumericos son: %i\n", no_althnu);
+    sentence[i] = '\0';
 
-    
+    while (sentence[i]!='\0')
+        
+            ++i;    
+        
+    printf("Len 2: %ld\n", i);
 
-    
+    puts("\n" MSG_USER_END);
+    puts(sentence);
+
+
     return 0;
 }
